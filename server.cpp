69fc4d5a5cc9bd78fca5de9e812877f0ff48461c
janeksmielowski <janek.smielowski@gmail.com>
2019-12-15 18:59:47 +0100

    Working clients accepting, kicking + bug fixes and console control

--- server.cpp
+++ server.cpp
@@ -12,111 +12,191 @@
 #include <algorithm>
 #include <string.h>
 
-#define ACCEPT "ACCEPT\0"
-#define REFUSE "REFUSE\0"
+#define ACCEPT "ACCEPT\n"
+#define REFUSE "REFUSE\n"
+#define KICK "KICK\n"
+
+#define COUNTDOWN_TIME 5
 #define MAX_LEN 8
 #define MAX_EVENTS 8
 
 using namespace std;
 const int one = 1;
 
-int sock;
+int serverSocket;
+bool isGame = false;
 vector<int> waitingClients, playingClients;
 
 thread newClientsThread, gameThread;
-mutex waitingClientMtx;
+mutex clientsMtx, isGameMtx;
 
 void handleNewConnections() {
     int clientSock;
     while(true) {
-        clientSock = accept(sock, nullptr, nullptr);
+        clientSock = accept(serverSocket, nullptr, nullptr);
 
         if (clientSock == -1) {
             perror("Client socket creation error");
             continue;
         }
 
+        printf("New client connected on socket %d\n", clientSock);
         write(clientSock, ACCEPT, sizeof(ACCEPT));
 
-        waitingClientMtx.lock();
-        waitingClients.push_back(clientSock);
-        waitingClientMtx.unlock();
+        isGameMtx.lock();
+        if (isGame) {
+            clientsMtx.lock();
+            playingClients.push_back(clientSock);
+            clientsMtx.unlock();
+        } else {
+            clientsMtx.lock();
+            waitingClients.push_back(clientSock);
+            clientsMtx.unlock();
+        }
+        isGameMtx.unlock();
+        
     }
 }
 
 void handleGame() {
     while (true) {
+        // wait for at least 2 clients
+        printf("Waiting for at least 2 connected clients\n");
+        int connectedClients = 0;
+        while (connectedClients < 2) {
+            clientsMtx.lock();
+            connectedClients = waitingClients.size() + playingClients.size();
+            clientsMtx.unlock();
+        }
+
+        printf("%d connected clients, preparing for a new game\n", connectedClients);
+
+        // notify game start
+        isGameMtx.lock();
+        isGame = true;
+        isGameMtx.unlock();
+
         // waitingClients moved from waiting room to new game
         // playingClients moved from last game to waiting room
-        waitingClientMtx.lock();
+        clientsMtx.lock();
         swap(waitingClients, playingClients);
-        waitingClientMtx.unlock();
+        clientsMtx.unlock();
 
-        // set epoll for new session countdown
+        // set epoll for a new session countdown
         int newGameEpoll = epoll_create1(0);
-
         epoll_event eevent, events[MAX_EVENTS];
         eevent.events = EPOLLIN;
-        eevent.data.fd = sock;
+        eevent.data.fd = serverSocket;
 
+        clientsMtx.lock();
         for (int i=0; i<waitingClients.size(); ++i)
             epoll_ctl(newGameEpoll, EPOLL_CTL_ADD, waitingClients.at(i), &eevent);
+        clientsMtx.unlock();
 
+        // set variables for countdown
         int eventCount = 0;
         char message[MAX_LEN];
         clock_t begClk = clock();
 
-        // run countdown to start new game session (NEEDS CHECK)
-        while ((clock() - begClk) / CLOCKS_PER_SEC < 30) {
+        // run countdown to start new game session
+        printf("Starting countdown\n");
+        int seconds = 0, elapsed = (clock() - begClk) / CLOCKS_PER_SEC;
+
+        while (elapsed < COUNTDOWN_TIME) {
             eventCount = epoll_wait(newGameEpoll, events, MAX_EVENTS, 0);
             
             for (int i=0; i<eventCount; ++i) {
-                message = read(events[i].data.fd, message, MAX_LEN);
+                int len = read(events[i].data.fd, message, MAX_LEN);
+                if (len == -1) {
+                    // ignore further client polling
+                    epoll_ctl(newGameEpoll, EPOLL_CTL_DEL, events[i].data.fd, NULL);
+                    continue;
+                }
+
                 printf("Request from socket %d: %s\n", events[i].data.fd, message);
 
                 // check request content
-                if (strcmp(message, ACCEPT) == 0) {
+                if (strncmp(message, ACCEPT, sizeof(ACCEPT)) == 0) {
                     
                     // move client from waiting to playing
-                    waitingClients.erase(remove(waitingClients.begin(), waitingClients.end(),
-                        events[i].data.fd), waitingClients.end());
+                    clientsMtx.lock();
+                    auto pos = find(waitingClients.begin(), waitingClients.end(), events[i].data.fd);
+                    if (pos != waitingClients.end()) waitingClients.erase(pos);
                     playingClients.push_back(events[i].data.fd);
+                    clientsMtx.unlock();
                     
                     // delete client's epoll
                     epoll_ctl(newGameEpoll, EPOLL_CTL_DEL, events[i].data.fd, NULL);
                 }
                 
             }
-            printf("%d\n", clock() - begClk);
+
+            elapsed = (clock() - begClk) / CLOCKS_PER_SEC;
+            if (elapsed > seconds)
+                printf("Time elapsed: %d\n", ++seconds);
+
         }
 
-        // TODO: kick clients that are not ready
-        // TODO: run game
+        close(newGameEpoll);
+
+        // kick waiting clients that didn't declare their readiness
+        printf("Checking waiting clients\n");
+        clientsMtx.lock();
+
+        if (waitingClients.size() == 0)
+            printf("All clients ready\n");
+        for (int i=0; i<waitingClients.size(); ++i) {
+            printf("Kicking client on socket %d\n", waitingClients.at(i));
+            write(waitingClients.at(i), KICK, sizeof(KICK));
+        }
+        waitingClients.clear();
+
+        // if not enough players
+        if (playingClients.size() < 2) {
+            printf("Not enough players, canceling game\n");
+            clientsMtx.unlock();
+            isGameMtx.lock();
+            isGame = true;
+            isGameMtx.unlock();
+            continue;
+        }
+
+        clientsMtx.unlock();
+
+        // TODO: implement game
+        printf("Starting new game\n");
+        printf("Game finished\n");
+
+        isGameMtx.lock();
+        isGame = false;
+        isGameMtx.unlock();
 
     }
 }
 
 int main() {
-    sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-    if (sock == -1) {
+    if (serverSocket == -1) {
         perror("Socket creation error");
         exit(EXIT_FAILURE);
     }
 
-    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
+    printf("Starting server on socket %d\n", serverSocket);
+    setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
 
     sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     addr.sin_port = htons(8080);
 
-    if (bind(sock, (sockaddr*) &addr, sizeof(addr)) == -1) {
+    if (bind(serverSocket, (sockaddr*) &addr, sizeof(addr)) == -1) {
         perror("Bind socket error");
         exit(EXIT_FAILURE);
     }
 
-    listen(sock, 1);
+    printf("Listening on 127.0.0.1, port 8080\n");
+    listen(serverSocket, 1);
 
     newClientsThread = thread(handleNewConnections);
     gameThread = thread(handleGame);
